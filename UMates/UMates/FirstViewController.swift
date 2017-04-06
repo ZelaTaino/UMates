70e4b2aa972c12b3bc0a8de481cbea0897837670
Taylor Ray Herald <THerald@taylors-mbp.dhcp.wustl.edu>
2017-04-06 17:47:18 -0500

    initial commit

---
+++ UMates/UMates/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  UMates
+//
+//  Created by Taylor Ray Herald on 4/6/17.
+//  Copyright © 2017 TheNerdHerd. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
